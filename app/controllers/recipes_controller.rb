4737ff708629193a9f5340713fd16ce8102e24af
ryu0929miki <118719477+ryu0929miki@users.noreply.github.com>
2023-01-18 14:47:40 +0900

    レシピの投稿機能（一部修正必要）

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -2,4 +2,17 @@ class RecipesController < ApplicationController
   def index
     @recipes = Recipe.all
   end
+
+  def new
+    @recipe = Recipe.new
+  end
+
+  def create
+    Recipe.create(recipe_params)
+  end
+
+  private
+  def recipe_params
+    params.require(:recipe).permit(:name, :image, :title, :material, :make)
+  end
 end
--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,2 +1,8 @@
 class Recipe < ApplicationRecord
+  has_one_attached :image
+
+
+  validates :title, presence: true
+  validates :material, presence: true
+  validates :make, presence: true
 end
