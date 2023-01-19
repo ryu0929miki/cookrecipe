499f1c23ebe98d30c70736d9ead59869054ae842
ryu0929miki <118719477+ryu0929miki@users.noreply.github.com>
2023-01-19 12:54:55 +0900

    レシピ削除機能

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -16,6 +16,11 @@ class RecipesController < ApplicationController
     end
   end
 
+  def destroy
+    recipe = Recipe.find(params[:id])
+    recipe.destroy
+  end
+
   private
   def recipe_params
     params.require(:recipe).permit(:name, :image, :title, :material, :make)
