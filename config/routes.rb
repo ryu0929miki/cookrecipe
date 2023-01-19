ec746771d228d6e8eb4a1071ce4324d80e6bac52
ryu0929miki <118719477+ryu0929miki@users.noreply.github.com>
2023-01-19 22:38:40 +0900

    コメント機能、検索機能実装

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,11 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'recipes#index'
-  resources :recipes
+  resources :recipes do
+   resources :comments, only: [:create, :destroy]
+   collection do
+     get 'search'
+   end
+  end
   resources :users, only: :show
 end
